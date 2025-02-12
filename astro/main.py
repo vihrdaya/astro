2bca8430badda181a789d6495a6b61e976d48380
kevin <kevin.rajkumar@outlook.com>
2025-02-12 13:41:40 -0500

    added facial landmarks

--- astro/main.py
+++ astro/main.py
@@ -1,4 +1,11 @@
 import cv2
+import mediapipe as mp
+
+# Initializing mediapipe face mesh
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
+mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
@@ -9,10 +16,22 @@ if not cap.isOpened():
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    
     if not ret:
         print("Failed to grab frame")
         break
+
+    # Convert the frame to RGB (MediaPipe uses RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with MediaPipe after conversion to RGB, because face_mesh.process() expects an rgb input
+    results = face_mesh.process(rgb_frame)
+
+    # Draw face landmarks
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, face_landmarks, mp_face_mesh.FACEMESH_TESSELATION)
 
     # Display the resulting frame
     cv2.imshow('Webcam Feed', frame)
